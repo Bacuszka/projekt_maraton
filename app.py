24b38432a6194e56e48c0ed56005b65b6af438c9
Baca <snyper1991@gmail.com>
2025-04-25 18:25:46 +0200

    nowe req

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pandas as pd
 import os
 import json
 import tempfile
-import base64
 from dotenv import load_dotenv
 from pycaret.regression import load_model, predict_model
 from openai import OpenAI
