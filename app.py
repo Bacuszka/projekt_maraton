a3d07cd970d399071ca365cb60622832513fb0bc
Baca <snyper1991@gmail.com>
2025-05-03 01:21:55 +0200

    poprawa app i ladowania modelu

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ from audio_recorder_streamlit import audio_recorder
 import requests
 import joblib
 import tempfile
+from pycaret.regression import load_model as pycaret_load_model
+import requests
+import tempfile
+import os
+import streamlit as st
 
 # 🔄 Wczytaj zmienne środowiskowe Langfuse
 load_dotenv()
@@ -46,18 +51,18 @@ def load_model_from_spaces():
         response = requests.get(MODEL_URL)
         response.raise_for_status()
 
-        # Zapisz do tymczasowego pliku
+        # Zapisz plik tymczasowo
         with tempfile.NamedTemporaryFile(delete=False, suffix=".pkl") as tmp_file:
             tmp_file.write(response.content)
             tmp_model_path = tmp_file.name
 
-        # Wczytaj model z pliku tymczasowego
-        model = joblib.load(tmp_model_path)
-        st.success("✅ Model został pomyślnie załadowany z DigitalOcean Spaces.")
+        # Wczytaj modelem z pycaret (NIE joblib)
+        model = pycaret_load_model(tmp_model_path.replace(".pkl", ""))
+        st.success("✅ Model został załadowany.")
         return model
 
     except Exception as e:
-        st.error(f"❌ Błąd podczas pobierania lub ładowania modelu: {e}")
+        st.error(f"❌ Błąd ładowania modelu z DigitalOcean: {e}")
         return None
 
 def recognize_speech_from_audio(audio_bytes):
